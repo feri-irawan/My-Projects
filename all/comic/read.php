458dc201f02aeb8dcdb93dd82b4d30e1a9cdc277
Feri Irawan <57158078+feri-irawan@users.noreply.github.com>
2021-04-17 05:10:06 +0800

    update read.php

--- all/comic/read.php
+++ all/comic/read.php
@@ -32,7 +32,7 @@ if (isset($_GET["chapter"])) {
     
     <ul class="list-group list-group-flush mb-3">
       <li class="list-group-item">Judul Comic: <?=$title?></li>
-      <li class="list-group-item"><?=$chapterTitle?></li>
+      <li class="list-group-item">Chapter: <?=$chapterTitle?></li>
       <li class="list-group-item">Jumlah halaman: <?=$comic->chapter_page?></li>
     </ul>
     
