12e8050680eabe1e0a1c3124a1b1b31f7ff8e26b
Feri Irawan <57158078+feri-irawan@users.noreply.github.com>
2021-04-16 21:13:30 +0800

    update index.php

--- all/comic/index.php
+++ all/comic/index.php
@@ -32,7 +32,7 @@ print_r($manga);
             </div>
             <ul class="list-group list-group-flush">
                 <li class="list-group-item">Jenis: <?=$row->type?></li>
-                <li class="list-group-item">Chapter: <?=$row->chapter?></li>
+                <li class="list-group-item"><?=$row->chapter?></li>
               </ul>
             <div class="card-body">
               <p class="card-text">
