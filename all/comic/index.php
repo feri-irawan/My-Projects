ced34f6574d5df3015d4819fcfe53d51ea886046
Feri Irawan <57158078+feri-irawan@users.noreply.github.com>
2021-04-17 06:36:29 +0800

    update index.php

--- all/comic/index.php
+++ all/comic/index.php
@@ -47,23 +47,14 @@ $manga = json_decode($manga);
     
 <nav aria-label="Page navigation">
   <ul class="pagination">
-    <?php if (isset($_GET["page"]) == 2): ?>
     <li class="page-item">
-      <a class="page-link" href="page=<?$_GET["page"] - 1?>" aria-label="Previous">
+      <a class="page-link" href="#" aria-label="Previous">
         <span aria-hidden="true">&laquo;</span>
       </a>
     </li>
-    <?php else: ?>
-    <li class="page-item">
-      <a class="page-link" href="page=<?$_GET["page"] - 1?>" aria-label="Previous">
-        <span aria-hidden="true">&laquo;</span>
-      </a>
-    </li>
-    <?php endif; ?>
     <li class="page-item"><a class="page-link" href="#">1</a></li>
     <li class="page-item"><a class="page-link" href="#">2</a></li>
     <li class="page-item"><a class="page-link" href="#">3</a></li>
-   
     <li class="page-item">
       <a class="page-link" href="#" aria-label="Next">
         <span aria-hidden="true">&raquo;</span>
