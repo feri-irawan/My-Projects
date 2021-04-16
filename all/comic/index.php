430b3f7e643841cb9cb8fd1f3e4f0bef20ce786c
Feri Irawan <57158078+feri-irawan@users.noreply.github.com>
2021-04-16 21:30:44 +0800

    update index.php

--- all/comic/index.php
+++ all/comic/index.php
@@ -28,7 +28,7 @@ $manga = json_decode($manga);
           <div class="card">
             <img src="<?=$row->thumb?>" class="card-img-top">
             <div class="card-body">
-              <a href="<?=$row->endpoint?>" target="_blank">
+              <a class="text-decoration-none" href="<?=$row->endpoint?>" target="_blank">
                 <h5 class="card-title"><?=$row->title?></h5>
               </a>
             </div>
