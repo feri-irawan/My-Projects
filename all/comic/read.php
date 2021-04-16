3339500fb5af66cc88ef9718b684a5c4639a49cc
Feri Irawan <57158078+feri-irawan@users.noreply.github.com>
2021-04-17 04:12:03 +0800

    update read.php

--- all/comic/read.php
+++ all/comic/read.php
@@ -31,7 +31,8 @@ if (isset($_GET["comic"])) {
     
     
   </section>
-  
+ 
+<?php if ($error === true): ?>
 <!-- Modal -->
 <div class="modal fade" id="errorAlert" tabindex="-1">
   <div class="modal-dialog">
@@ -50,6 +51,7 @@ if (isset($_GET["comic"])) {
     </div>
   </div>
 </div>
+<?php endif; ?> 
   
   
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
