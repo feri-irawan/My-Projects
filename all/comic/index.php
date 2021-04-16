02a0e07cc39c2725717add91e900127b78b4856b
Feri Irawan <57158078+feri-irawan@users.noreply.github.com>
2021-04-16 17:41:50 +0800

    update index.php

--- all/comic/index.php
+++ all/comic/index.php
@@ -1,1 +1,37 @@
-This comic page
+<?php
+if (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && ($_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest')) {
+ if (isset($_GET['character-id'])) {
+  $curl = curl_init();
+  curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+
+  $character_id = $_GET['character-id'];
+
+  $ts = time();
+  $public_key = 'cfa476604e04fd6e0bc9c86eb904badc';
+  $private_key = '983e3d08023e136611550d3eca68d89366482d1f';
+  $hash = md5($ts . $private_key . $public_key);
+
+  $query = array(
+   'format' => 'comic',
+   'formatType' => 'comic',
+   'apikey' => $public_key,
+   'ts' => $ts,
+   'hash' => $hash,
+  );
+
+  curl_setopt($curl, CURLOPT_URL,
+   "https://gateway.marvel.com:443/v1/public/characters/" . $character_id . "/comics" . "?" . http_build_query($query)
+  );
+
+  $result = json_decode(curl_exec($curl), true);
+
+  curl_close($curl);
+
+  echo json_encode($result);
+
+ } else {
+  echo "Character Id not defined";
+ }
+} else {
+ echo "Error: wrong server.";
+}
