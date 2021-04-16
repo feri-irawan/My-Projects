9b5d931754886e2fc294c96f6b7638b93fe7373a
Feri Irawan <57158078+feri-irawan@users.noreply.github.com>
2021-04-16 18:11:09 +0800

    update index.php

--- all/comic/index.php
+++ all/comic/index.php
@@ -21,3 +21,38 @@ $response_data = json_decode($response, true);
 
 //print
 print_r($response_data);
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+
+  <title>Comic</title>
+</head>
+<body>
+  
+  <div class="row">
+      
+  </div>
+  <?php foreach ($response_data["data"]["results"] as $comic): ?>
+    <div class="col-6">
+      <div class="card" style="width: 18rem;">
+        <img src="<?=$comic['thumbnail']['path'].".".$comic['thumbnail']['extension']?>" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+    </div>
+  <?php endforeach; ?>
+  
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+</body>
+</html>
