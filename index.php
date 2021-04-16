9312f1a23c0b7192a9a840c447ae9ae4136718ef
Feri Irawan <57158078+feri-irawan@users.noreply.github.com>
2021-04-16 14:33:13 +0800

    update index.php

--- index.php
+++ index.php
@@ -18,6 +18,11 @@ $projects = json_decode(file_get_contents("projects.json"), true);
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
 
   <title>Add Project List</title>
+  <style>
+  a {
+    text-decoration: none;
+  }
+  </style>
 </head>
 <body>
   
