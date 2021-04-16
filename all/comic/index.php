a56a7f39d0c256a83542b6fa117aaed9cc6dabc0
Feri Irawan <57158078+feri-irawan@users.noreply.github.com>
2021-04-17 06:59:33 +0800

    update index.php

--- all/comic/index.php
+++ all/comic/index.php
@@ -45,8 +45,8 @@ $manga = json_decode($manga);
         
     </div>
     
-<nav class="text-center" aria-label="Page navigation">
-  <ul class="pagination">
+<nav aria-label="Page navigation">
+  <ul class="pagination justify-content-center">
     <li class="page-item">
       <a class="page-link" href="page=<?=(isset($_GET["page"]) > 1) ? $_GET["page"] - 1 : "#"; ?>" aria-label="Previous">
         <span aria-hidden="true">&laquo;</span>
