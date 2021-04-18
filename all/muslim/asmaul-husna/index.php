65e03b9586053a83e93bb1ad8c0afb496c1ae133
Feri Irawan <57158078+feri-irawan@users.noreply.github.com>
2021-04-18 17:44:29 +0800

    update index.php

--- all/muslim/asmaul-husna/index.php
+++ all/muslim/asmaul-husna/index.php
@@ -54,7 +54,9 @@ $data = json_decode($data);
       </div>
     </section>
     
-    
+    <footer class="text-center p-3">
+      <small>&copy; <?=date("Y")?> Made with &hearts; by <a href="https://feri-irawan.netlify.app" class="text-decoration-none">Feri Irawan</a></small>
+    </footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
   </body>
 </html>
