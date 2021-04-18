6ae1eb1d2de785bb1810e6b1c9b28c613b80e7a4
Feri Irawan <57158078+feri-irawan@users.noreply.github.com>
2021-04-18 17:29:25 +0800

    update index.php

--- all/muslim/asmaul-husna/index.php
+++ all/muslim/asmaul-husna/index.php
@@ -33,8 +33,8 @@ $data = json_decode($data);
               No. <?=$row->index?>
             </div>
             <div class="card-body">
-              <div class="row align-items-center">
-                <div class="col-6">
+              <div class="row">
+                <div class="col-6 align-items-center">
                   <h5><?=$row->latin?></h5><br>
                 </div>
                 
