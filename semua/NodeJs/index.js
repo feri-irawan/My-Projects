7240b86f6f42c6e00739ab9dfa88bd4fd724e4f8
Feri Irawan <57158078+feri-irawan@users.noreply.github.com>
2021-04-18 09:34:48 +0800

    Update index.js

--- semua/NodeJs/index.js
+++ semua/NodeJs/index.js
@@ -1,9 +1,8 @@
 var http = require('http');
-
-var server = http.createServer(function (req, res) {
-    res.end("Hi, selamat datang di nodejs");
-});
-
-server.listen(8000);
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('Hello <b>World</b>!');
+    res.end();
+}).listen(8000);
 
 console.log("server running on http://localhost:8000");
