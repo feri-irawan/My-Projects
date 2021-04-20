1613bba05e4bcebe7c1649225f2d3247ae147101
Feri Irawan <57158078+feri-irawan@users.noreply.github.com>
2021-04-20 15:17:04 +0800

    update index.php

--- all/comic/index.php
+++ all/comic/index.php
@@ -1,10 +1,15 @@
 <?php
-$endPoint = "https://mangamint.kaedenoki.net/api/manga/";
+$endPoint = "https://mangamint.kaedenoki.net/api/";
 $pageNumber = (isset($_GET["page"])) ? $_GET["page"] : 1;
-$manga = file_get_contents("{$endPoint}page/{$pageNumber}");
+$manga = file_get_contents("{$endPoint}manga/page/{$pageNumber}");
 $manga = json_decode($manga);
 
 //print_r($manga);
+
+// Genre
+$genres = file_get_contents("{$endPoint}genres");
+$genres = json_decode($genres);
+
 ?>
 <!doctype html>
 <html lang="en">
@@ -27,7 +32,44 @@ $manga = json_decode($manga);
   </style>
 </head>
 <body>
-  
+  <header>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Genre(s)
+              </a>
+              <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li><a class="dropdown-item" href="#">Action</a></li>
+                <li><a class="dropdown-item" href="#">Another action</a></li>
+                <li><hr class="dropdown-divider"></li>
+                <li><a class="dropdown-item" href="#">Something else here</a></li>
+              </ul>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+            </li>
+          </ul>
+          <form class="d-flex">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success" type="submit">Search</button>
+          </form>
+        </div>
+      </div>
+    </nav>
+  </header>
   <section class="container p-3">
     <div class="row">
         
