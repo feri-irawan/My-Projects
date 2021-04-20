024181872d214dcf540410e7250597822bdcef35
Feri Irawan <57158078+feri-irawan@users.noreply.github.com>
2021-04-20 12:33:37 +0800

    update index.php

--- all/comic/index.php
+++ all/comic/index.php
@@ -17,6 +17,14 @@ $manga = json_decode($manga);
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
 
   <title>Comic</title>
+  
+  <style>
+    @media and (min-width: 768px) {
+      :root {
+        font-size: 10px;
+      }
+    }
+  </style>
 </head>
 <body>
   
