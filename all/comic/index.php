97e1b6919dbf2395895674ff288190a21945f56b
Feri Irawan <57158078+feri-irawan@users.noreply.github.com>
2021-04-21 17:09:51 +0800

    update index.php

--- all/comic/index.php
+++ all/comic/index.php
@@ -52,7 +52,7 @@ $genres = json_decode($genres);
           </svg>
           Comic
           </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler border-0" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
