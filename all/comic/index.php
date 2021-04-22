15c1dc2dc1dbb283d124d95459f1a98b6194939b
Feri Irawan <57158078+feri-irawan@users.noreply.github.com>
2021-04-22 12:17:13 +0800

    update index.php

--- all/comic/index.php
+++ all/comic/index.php
@@ -28,7 +28,8 @@ $genres = json_decode($genres);
   <!-- Required meta tags -->
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-
+  <meta name="google" content="notranslate">
+  
   <!-- Bootstrap CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
 
