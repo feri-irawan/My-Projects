09efead21e11f92163b3193a3f9505cc08a761a0
Feri Irawan <57158078+feri-irawan@users.noreply.github.com>
2021-04-26 22:23:18 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,3 @@
-<?php
-/**
-* =======================
-* Hi, welcome to my all projects repository
-* =======================
-*/
-
-//Default
-$projects = json_decode(file_get_contents("projects.json"), true);
-?>
-
-
 <!doctype html>
 <html lang="en">
 <head>
@@ -17,75 +5,45 @@ $projects = json_decode(file_get_contents("projects.json"), true);
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
 
-  <title>Add Project List</title>
+  <title>PROJECTS</title>
 </head>
 <body>
   
   <section class="container p-3">
   <h5>PROJECTS</h5>
-  <ol class="list-group list-group-numbered">
-    <?php foreach ($projects as $project): ?>
-    <li class="list-group-item list-group-item-action">
-      <a href="<?= $project["url"] ?>" class="text-decoration-none">
-        <?= $project["name"] ?>
-      </a>
-    </li>
-    <?php endforeach; ?>
+  <ol class="list-group list-group-numbered" id="list-projects">
   </ol>
-  
-  
-  
-  
-  <?php
-  /**
-   * Form Add Project
-   */
-  if (isset($_GET["add-project"])): ?>
-    <?php if (isset($_GET["success-add"])): ?>
-      <div class="alert alert-success">
-        Has successfully added a new project to the list!
-      </div>
-    <?php endif; ?>
-    
-    <form action="" method="post">
-      <div class="input-group mb-3">
-        <span class="input-group-text">Name</span>
-        <input name="input-name" type="text" class="form-control" required>
-      </div>
-      <div class="input-group mb-3">
-        <span class="input-group-text">URL</span>
-        <input name="input-url" type="text" class="form-control" required>
-      </div>
-      <button name="btn-submit" type="submit" class="btn btn-secondary">Save</button>
-    </form>
-    
-    <?php
-    if (isset($_POST["btn-submit"])) {
-      $name = $_POST["input-name"];
-      $url = $_POST["input-url"];
-      
-      $json_url = "projects.json";
-      $data = file_get_contents($json_url);
-      $data = json_decode($data, true);
-      
-      $query = [
-        "name" => $name,
-        "url" => $url
-        ];
-      
-      $data[] = $query;
-      
-      $data = json_encode($data, JSON_PRETTY_PRINT);
-      file_put_contents($json_url, $data);
-     
-      header("Location: index.php?add-project&success-add");
-      exit();
-    }
-    ?>
-  <?php endif; ?>
-  
-  
   </section>
+  
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js" type="text/javascript" charset="utf-8"></script>
+  <script>
+    $(document).ready(function () {
+    
+      let container = $("#list-projects");
+      
+      // loading span
+      container.html("Loading...");
+      
+      $.ajax({
+        url: "https://raw.githubusercontent.com/feri-irawan/My-Projects/main/projects.json",
+        type: "get",
+        dataType: "json",
+        success: function(hasil) {
+        
+        // clear container before append
+        container.html("");
+        
+        $.each(hasil, function (i, project) {
+          container.append(`
+              <li class="list-group-item list-group-item-action">
+                <a href="`+project.url+`" class="text-decoration-none">`+project.name+`</a>
+              </li>
+            `);
+          });
+        }
+      });
+    });
+  </script>
 </body>
 </html>
