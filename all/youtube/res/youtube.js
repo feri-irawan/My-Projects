d8defc0e1062806e0cfc986c6f5c371892d8e842
Feri Irawan <57158078+feri-irawan@users.noreply.github.com>
2021-05-04 13:23:14 +0800

    update youtube.js

--- all/youtube/res/youtube.js
+++ all/youtube/res/youtube.js
@@ -71,4 +71,3 @@ function dateFormat(date) {
   var date = new Date(date);
   return ((date.getMonth() > 8) ? (date.getMonth() + 1) : ('0' + (date.getMonth() + 1))) + '/' + ((date.getDate() > 9) ? date.getDate() : ('0' + date.getDate())) + '/' + date.getFullYear()
 }
-!
